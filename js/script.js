0f6d58f284397f8095225d5f14a18408eb8676ac
constWardtz <edwardtutorials22@gmail.com>
2020-12-07 07:27:13 -0400

    final

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-const toggleNavbar = document.querySelector(`#toggle-navbar`)
-const navbarMobile = document.querySelector(`#navbar-mobile`)
+const toggleNavbar = document.querySelector(`#toggle-navbar`);
+const navbarMobile = document.querySelector(`#navbar-mobile`);
+const shortUrl = document.querySelector(`#shortUrl`);
+const urlField = document.querySelector(`#urlField`).value;
 
-const burgerBtn = 'fa-bars'
-const closeBtn = 'fa-close'
+const burgerBtn = "fa-bars";
+const closeBtn = "fa-close";
 
-
+/* Navbar */
 toggleNavbar.addEventListener(`click`, () => {
-    if (toggleNavbar.classList.contains(burgerBtn)) {
-        toggleNavbar.classList.remove(burgerBtn)
-        toggleNavbar.classList.add(closeBtn)
+  if (toggleNavbar.classList.contains(burgerBtn)) {
+    toggleNavbar.classList.remove(burgerBtn);
+    toggleNavbar.classList.add(closeBtn);
 
-        navbarMobile.style = `
+    navbarMobile.style = `
             display: grid;
-        `
-    } else if (toggleNavbar.classList.contains(closeBtn)) {
-        toggleNavbar.classList.add(burgerBtn)
-        toggleNavbar.classList.remove(closeBtn)
+        `;
+  } else if (toggleNavbar.classList.contains(closeBtn)) {
+    toggleNavbar.classList.add(burgerBtn);
+    toggleNavbar.classList.remove(closeBtn);
 
-        navbarMobile.style = `
+    navbarMobile.style = `
             display: none;
-        `
-   }
-})
+        `;
+  }
+});
